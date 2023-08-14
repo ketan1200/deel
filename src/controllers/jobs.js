358015e5562ab4a6ea0a15c2e59fcfcaeb76ad2b
= <=>
2023-08-14 11:54:20 +0530

    Implemented Payment route for specific job

--- src/controllers/jobs.js
+++ src/controllers/jobs.js
@@ -26,5 +26,65 @@ async function getUnpaidJobs(req, res) {
 
 }
 
+async function payForJob(req, res) {
 
-module.exports = { getUnpaidJobs };
+  let DBTransaction;
+
+  const profile = req.profile;
+  const jobId = req.params.job_id;
+  const profileId = req.profile.id;
+
+  try {
+
+    if (profile.type !== 'client') return res.status(400).json({ message: 'Invalid Request' });
+
+    const jobDetails = await Models.Job.findOne({
+      where: { id: jobId },
+      include: [{
+        attributes: ['ContractorId'],
+        model: Models.Contract,
+        where: {
+          "ClientId": profileId
+        }
+      }],
+    });
+
+    if (!jobDetails) return res.status(400).json({ message: 'Could not found requested job' });
+
+    if (jobDetails.paid) return res.status(400).json({ message: 'You have already paid for this job' });
+
+    if (jobDetails.price > req.profile.balance) return res.status(400).json({ message: 'Insufficient balance in your account, Please deposit money' });
+
+    DBTransaction = await Models.sequelize.transaction({ autocommit: false });
+
+    const contractor = await Models.Profile.findOne({ where: { type: 'contractor', id: jobDetails.Contract.ContractorId } });
+    const client = await Models.Profile.findOne({ where: { type: 'client', id: profileId }, lock: true, transaction: DBTransaction });
+
+    const updatedBalanceForClient = profile.balance - jobDetails.price;
+    const updatedBalanceForContractor = contractor.balance + jobDetails.price;
+
+    await Models.Profile.update({ balance: updatedBalanceForClient }, { where: { id: client.id }, transaction: DBTransaction });
+
+    await Models.Profile.update({ balance: updatedBalanceForContractor }, { where: { id: contractor.id }, transaction: DBTransaction });
+
+    await Models.Job.update({ paid: true, paymentDate: new Date().toISOString() }, { where: { id: jobId }, transaction: DBTransaction });
+
+    await DBTransaction.commit();
+
+    res.status(200).send(jobDetails);
+
+  } catch (error) {
+
+    if (DBTransaction) DBTransaction.rollback();
+
+    let message = 'Something went wrong, Please try again later';
+
+    if (error?.parent?.code === 'SQLITE_BUSY') message = 'Request already in-progress, Please try again after sometime.'
+
+    return res.status(500).json({ message });
+
+  }
+
+}
+
+module.exports = { getUnpaidJobs, payForJob };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { getProfile } = require("./middleware/getProfile");
 
 const { getContractById, getContracts } = require('./controllers/contracts');
-const { getUnpaidJobs } = require('./controllers/jobs');
+const { getUnpaidJobs, payForJob } = require('./controllers/jobs');
 
 const router = express.Router();
 
@@ -13,6 +13,6 @@ router.get('/contracts/', getProfile, getContracts);
 
 router.get('/jobs/unpaid', getProfile, getUnpaidJobs);
 
-
+router.get('/jobs/:job_id/pay', getProfile, payForJob);
 
 module.exports = router;
